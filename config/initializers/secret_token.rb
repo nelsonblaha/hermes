f4f371b0022406bd7d38783a219d33c366e6ae4c
Nelson Blaha <nelson.blaha@gmail.com>
2012-12-01 12:58:30 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hermes::Application.config.secret_token = '4300b3318fd2d0da4913b914ed22ccaefbf20642f7729dded6589026bce6f532e8330761676ec4a0e0ee97e6f6243013485b32672ce274ee9aa5e4c358a24708'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Hermes::Application.routes.draw do
+  
+  root :to => 'home#index'
+
+end
