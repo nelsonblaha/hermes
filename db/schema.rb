787331587f1ec9a885d2ae5d3bac9763a14e56f4
Nelson Blaha <nelson.blaha@gmail.com>
2012-12-29 12:51:37 -0600

    update gitignore to avoid disclosing beta credentials

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121224010548) do
+ActiveRecord::Schema.define(:version => 20121229184108) do
 
   create_table "inboxes", :force => true do |t|
     t.string   "name"
@@ -68,6 +68,14 @@ ActiveRecord::Schema.define(:version => 20121224010548) do
     t.datetime "created_at",                  :null => false
     t.datetime "updated_at",                  :null => false
     t.integer  "mode",         :default => 1
+  end
+
+  create_table "twitters", :force => true do |t|
+    t.string   "uid"
+    t.integer  "user_id"
+    t.string   "token"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
