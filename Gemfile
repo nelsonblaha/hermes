bd9e4b853dff22c25de80f6cb20b4eb72a67f453
Nelson Blaha <nelson.blaha@gmail.com>
2013-03-17 19:40:07 -0500

    Ties application session secret to Figaro

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'devise'
 #for parsing rss feeds
   gem 'feedzirra'
 gem 'figaro'
+gem 'omniauth-twitter'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Hermes::Application.config.secret_token = '4300b3318fd2d0da4913b914ed22ccaefbf20642f7729dded6589026bce6f532e8330761676ec4a0e0ee97e6f6243013485b32672ce274ee9aa5e4c358a24708'
+Hermes::Application.config.secret_token = ENV["APPLICATION_SECRET_TOKEN"]
