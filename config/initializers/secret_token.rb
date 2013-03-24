2e5bf02cc288cd0736dc546b8fd67736a88efa60
Nelson Blaha <nelson.blaha@gmail.com>
2013-03-24 14:26:22 -0500

    Derive secret token from Figaro's application.yml

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
+puts ENV["APPLICATION_SECRET_TOKEN"]
 Hermes::Application.config.secret_token = ENV["APPLICATION_SECRET_TOKEN"]
