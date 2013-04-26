824a537c289dd3a8c3e953bbeaa1c332b42ed2a5
Nelson Blaha <nelson.blaha@gmail.com>
2013-04-26 18:43:24 -0500

    command modal that doesn't do anything yet

--- app/assets/javascripts/inboxes.js
+++ app/assets/javascripts/inboxes.js
@@ -3,18 +3,25 @@ $(document).ready(function(){
   $('div.message').first().addClass('selected');
 
   $('html').keypress(function(e){
-    e.preventDefault();
-    // down key j
-    if (e.which == 106 && $("div.selected").next().hasClass('message')){
-      $('div.selected').removeClass('selected').next().addClass('selected');
-    }
-    // up key k
-    if (e.which == 107 && $("div.selected").prev().hasClass('message')){
-      $('div.selected').removeClass('selected').prev().addClass('selected');
-    }
-    // command dialog toggle a
-    if (e.which == 97 && $("div.selected").length > 0) {
-      $('div#command').modal("toggle");
+    //allow letters through if input is focus
+    if ($(e.target).is('input')) { return true; }
+    else {
+      e.preventDefault();
+      // down key j
+      if (e.which == 106 && $("div.selected").next().hasClass('message')){
+        $('div.selected').removeClass('selected').next().addClass('selected');
+      }
+      // up key k
+      if (e.which == 107 && $("div.selected").prev().hasClass('message')){
+        $('div.selected').removeClass('selected').prev().addClass('selected');
+      }
+      // command dialog toggle a
+      if (e.which == 97 && $("div.selected").length > 0) {
+        $('div#command').modal("toggle");
+        window.setTimeout(function(){
+          $('div#command input').focus();
+        },300);
+      }
     }
   });
 });
